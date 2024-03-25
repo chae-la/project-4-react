0e6853db7c8de0389753de93e1de251d1a32fdb2
Michaela <michaelachan45@gmail.com>
2024-03-25 15:39:10 +0000

    styled tablet and laptop media queries

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,16 @@ import team from "./data/team";
 import Card from "./components/Card/Card";
 
 const App = () => {
+  
   return (
     <div className="app">
-      <h1 className="app__title">Ticket Tracker</h1>
-      <Card />
+        <h1 className="app__title">Ticket Tracker</h1>
+      <div className="app__counter-container">
+      {team.map(member => (
+        <Card key={member.id} name={member.name} role={member.role} />
+      ))}
+
+      </div>
     </div>
   );
 };
